c318d514542700cc78dce21dd0ef6b0680f7f55e
LAPTOP-GJ2SP6GC\ernestoOlaez <olaez_ernesto@hotmail.com>
2021-11-05 16:42:10 -0600

    Add Class and Project Selenium to SDET

---
+++ basicSelenium/launchBrowser/Chrome_Launch.java
@@ -0,0 +1,24 @@
+package launchBrowser;
+
+import java.sql.Driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class Chrome_Launch {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",".\\driver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		
+	   driver.get("https://opensource-demo.orangehrmlive.com/");
+
+	}
+
+}
