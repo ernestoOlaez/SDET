d03cc1d247805356469d0c140e06d0aa8f2f47ab
LAPTOP-GJ2SP6GC\ernestoOlaez <olaez_ernesto@hotmail.com>
2022-01-07 16:05:39 -0600

    Add a JavaBasics Folder

---
+++ JavaBasics/DataTypesAndVariables/BooleanTestExercise.java
@@ -0,0 +1,24 @@
+package DataTypesAndVariables;
+
+//Ernesto  Olaez
+
+public class BooleanTestExercise {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		//Use the boolean keyword to declare boolean variable.
+		boolean testResult=true;
+		
+		System.out.println("Test Result: " + testResult);
+		
+		//Change the value for boolean variable
+		testResult=false;
+		
+		System.out.println("Test Result: "+ testResult);
+		
+
+	}
+
+}
