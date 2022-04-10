6c33e4826856991622db704cc066ef40efd77d9f
LAPTOP-GJ2SP6GC\ernestoOlaez <olaez_ernesto@hotmail.com>
2022-04-09 20:28:46 -0500

    Add new class  Codity

---
+++ Codity/Iterations/Loops1.java
@@ -0,0 +1,16 @@
+package Iterations;
+
+public class Loops1 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		for (int i=1; i<100; i++)
+		{
+			System.out.println("i: " +i);
+		}
+
+	}
+
+}
---
+++ JavaBasics/DataTypesAndVariables/IntegerTestExercise.java
@@ -0,0 +1,26 @@
+package DataTypesAndVariables;
+
+public class IntegerTestExercise {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		//  Use the int keyword to declare integer variable
+		int carSpeed;
+		
+		//Initialize the integer variable with value 20
+		carSpeed=20;
+		
+		//Print the value for integer variable.
+		
+		System.out.println("Car is running at the speed of: "+ carSpeed);
+		
+		//Change the value of integer variable
+		carSpeed=carSpeed+20;
+		
+		
+		//Print the value for integer variable.
+		System.out.println("Car is running at the speed of: " +carSpeed);
+	}
+
+}
