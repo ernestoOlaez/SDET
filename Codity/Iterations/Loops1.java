6a19b06be8e9c57e38f54132081918713bc092a1
LAPTOP-GJ2SP6GC\ernestoOlaez <olaez_ernesto@hotmail.com>
2022-04-09 20:50:32 -0500

    ok

--- Codity/Iterations/Loops1.java
+++ Codity/Iterations/Loops1.java
@@ -5,10 +5,12 @@ public class Loops1 {
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
+		System.out.println("Loop 1");
 		
 		for (int i=1; i<100; i++)
 		{
 			System.out.println("i: " +i);
+			
 		}
 
 	}
