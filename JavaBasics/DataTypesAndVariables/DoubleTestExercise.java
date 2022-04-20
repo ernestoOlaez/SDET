a243affd033a1e2f2db9a1b04351a70928e529c3
LAPTOP-GJ2SP6GC\ernestoOlaez <olaez_ernesto@hotmail.com>
2022-04-20 11:59:04 -0500

    Add new class for Variable example

---
+++ JavaBasics/DataTypesAndVariables/DoubleTestExercise.java
@@ -0,0 +1,17 @@
+package DataTypesAndVariables;
+
+public class DoubleTestExercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double PI;
+		
+		PI=3.14159;
+		
+		//Print the value of double variable
+		System.out.println("PI: "+ PI);
+
+	}
+
+}
